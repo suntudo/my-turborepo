eb71fedd3214463d878de5bb5618e5beccf8a9c1
suntudo <71748510+suntudo@users.noreply.github.com>
2023-04-19 17:12:40 -0400

    update nextjs config with redirects and rewrites

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,27 @@
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ["ui"],
+  rewrites() {
+    beforeFiles: [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "downloads.sunhouse.tech",
+          },
+        ],
+        destination: "/downloads/:path*",
+      },
+    ];
+  },
+  redirects() {
+    return [
+      {
+        source: "/home",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
 };
