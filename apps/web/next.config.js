c858da78bb48b87d99c8021daa9413b6095cc3ac
suntudo <71748510+suntudo@users.noreply.github.com>
2023-05-01 13:33:39 -0400

    trying new redirect

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,28 +1,38 @@
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ["ui"],
-  rewrites() {
-    return {
-      beforeFiles: [
-        {
-          source: "/:path*",
-          has: [
-            {
-              type: "host",
-              value: "downloads.sunhouse.tech",
-            },
-          ],
-          destination: "/downloads/:path*",
-        },
-      ],
-    };
-  },
-  redirects() {
+  // rewrites() {
+  //   return {
+  //     beforeFiles: [
+  //       {
+  //         source: "/:path*",
+  //         has: [
+  //           {
+  //             type: "host",
+  //             value: "downloads.sunhouse.tech",
+  //           },
+  //         ],
+  //         destination: "/downloads/:path*",
+  //       },
+  //     ],
+  //   };
+  // },
+  async redirects() {
     return [
       {
         source: "/home",
         destination: "/",
         permanent: true,
+      },
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "downloads.sunhouse.tech",
+          },
+        ],
+        destination: "https://sunhouse.tech/downloads/:path*",
       },
     ];
   },
